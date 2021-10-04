c51c08df2bdf96bee840cc8056c6d1281806c637
praybe <91723715+praybe@users.noreply.github.com>
2021-10-04 19:12:48 +0900

    Add files via upload

---
+++ eclipse-workspace/HelloWorld2/src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("AAA");
+		System.out.println(1 + 1);
+
+
+	}
+
+}
---
+++ eclipse-workspace/HelloWorld2/src/Hello1.java
@@ -0,0 +1,11 @@
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		System.out.println("AAA");
+		System.out.println(1 + 1);
+
+
+	}
+
+}
