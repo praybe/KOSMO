db87a18d01e62a65639d531d09074dcb67f9e062
praybe <91723715+praybe@users.noreply.github.com>
2021-10-12 11:27:38 +0900

    Add files via upload

---
+++ eclipse-workspace/Homework/211008/src/Num15.java
@@ -0,0 +1,18 @@
+//15.아래와 같이 출력 하시오.
+public class Num15 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+
+			for (int j = 1; j <= 5; j++) {
+				
+				System.out.print(i + j + " ");
+
+			}
+			System.out.println();
+		}
+
+	}
+
+}
