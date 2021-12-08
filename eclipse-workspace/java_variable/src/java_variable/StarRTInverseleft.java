587e8f7ac1fee41ab0953968c3fc6d057079b9e6
praybe <91723715+praybe@users.noreply.github.com>
2021-12-08 18:42:15 +0900

    Update StarRTInverseleft.java

--- eclipse-workspace/java_variable/src/java_variable/StarRTInverseleft.java
+++ eclipse-workspace/java_variable/src/java_variable/StarRTInverseleft.java
@@ -5,13 +5,13 @@ public class StarRTInverseleft {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 5; i++) {
 
-			for (int j = 1; j < i; j++) { // 공백 담당 for (int j = 4; j >= i; j--)가능
+			for (int j = 1; j < i; j++) { // 공백 담당
 
 				System.out.print(" ");
 
 			}
 
-			for (int j = i; j <= 5; j++) { // 별 담당 위에가 j--일경우 for (int j = 1; j <= i; j++)이걸로
+			for (int j = i; j <= 5; j++) { // 별 담당 for (int j = 5; j >= i; j--)도 가능
 
 				System.out.print("*");
 			}
